0e1f451d127c3f1293ff8ac916acdd9ec5c4414d
yunimaxxik <nicxefonn@yandex.ru>
2026-07-26 19:01:55 +0300

    Настроил приложение для публикации на GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,15 @@ import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'node:url';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  const isProd = mode === 'production';
+  return {
+    base: isProd ? '/todo-react/' : '/',
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
-  }
+  };
 });
